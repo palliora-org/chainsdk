742146c2c4640eb46ad1ca93c4ac36c2bd782d04
Ujjwal Kumar <31813384+Ujjwal0501@users.noreply.github.com>
2026-03-11 21:17:21 +0530

    chore: add build support for cjs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-export * from './chain';
-export * from './compute';
-export * from './crypto';
-export * from './da';
-export * from './guardian';
-export * from './stake';
-export * from './token';
-export * from './utils';
-export * from './validator';
-export * from './config';
-export * from './identity';
-export * from './rotateKeys';
+export * from "./chain";
+export * from "./compute";
+export * from "./crypto";
+export * from "./da";
+export * from "./guardian";
+export * from "./stake";
+export * from "./token";
+export * from "./utils";
+export * from "./validator";
+export * from "./config";
+export * from "./identity";
+export * from "./rotateKeys";
+
+export * from "@polkadot/util";
+export { ApiPromise, WsProvider } from "@polkadot/api";;
