adbfe0e8e471ad1df9250606ae3c00b99b09e649
Ujjwal Kumar <31813384+Ujjwal0501@users.noreply.github.com>
2026-03-19 10:39:41 +0530

    chore: update payout to batch call on eras

--- src/stake/payout.ts
+++ src/stake/payout.ts
@@ -1,18 +1,21 @@
 import { getApi, signAndSend } from "../chain";
 import { assert, debugLog } from "../utils";
 
-export async function payoutStake(account: any, eras: Array<number>) {
+export async function payoutStake(account: any, eras: Array<number>, address?: string) {
   const api = await getApi();
 
   assert(api, "API not initialized");
   assert(account, "Account not initialized");
 
-for (const era of eras) {
-    debugLog("Paying account: ", account.address, " for era ", era);
-
-    const unstakeTx = api.tx.staking.payoutStakers(account.address, era);
-    const hash = await signAndSend(unstakeTx, account);
-
-    debugLog(`Unstake transaction sent with hash: ${hash.hash}`);
+  for (const era of eras) {
+    debugLog("Paying account: ", address || account.address, " for era ", era);
   }
+
+  const unstakeTxs = eras.map((era) =>
+    api.tx.staking.payoutStakers(address || account.address, era)
+  );
+  const batchTx = api.tx.utility.batch(unstakeTxs);
+  const hash = await signAndSend(batchTx, account);
+
+  debugLog(`Batch payout transaction sent with hash: ${hash.hash}`);
 }
